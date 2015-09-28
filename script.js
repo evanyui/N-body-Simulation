16ce79dc9b5f76bf99ef4afd6be0098543f32691
evanyui <evanyui@hotmail.com>
2015-09-28 03:27:28 -0400

    size changes color, control changed to drag and drop, deleted some attributes such as speed, accel, degree

--- script.js
+++ script.js
@@ -12,14 +12,20 @@ var particles = [];
 var mouseX, mouseY;
 
 //constants
-var pi = Math.PI;
+var PI = Math.PI;
+var VLC_SCALE = 10;
 
-//Event handlers
-document.addEventListener("click", onClick);
+//Event handlers NOT USED YET
+//document.addEventListener("click", onClick);
 
-//convert degree to radian
+//convert degree to radian *THIS IS NOT USED
 function getRad(deg) {
-  	return deg * Math.PI/180;
+  	return deg * PI/180;
+}
+
+//Convert radian to degree
+function getDeg(rad) {
+	return rad * 180/PI;
 }
 
 //Get sign according to angular graph, produce opposite Y sign because of PC screen Y axis is opposite
@@ -29,22 +35,28 @@ function getSign(value) {
 	var signXY = [];
 
 	//Cases for 4 different zone of angular graph to decide the sign of x and y components
-	if(value<=90) {
+	//NOTE: because computer screen starts with left top (0,0), 
+ 		//instead of table A,S,T,C , computer has -A,-S,S,A (0,-90,180,90)
+ 	//first zone
+	if(value<=0 && value>=-90) {
 		signX = 1;
 		signY = -1;
 		signXY.push({x:signX,y:signY});
 	}
-	else if(value<=180 && value>90) {
+	//second zone
+	else if(value<=-90 && value>=-180) {
 		signX = -1;
 		signY = -1;
 		signXY.push({x:signX, y:signY});
 	}
-	else if(value<=270 && value>180) {
+	//third zone
+	else if(value>=90 && value<=180) {
 		signX = -1;
 		signY = 1;
 		signXY.push({x:signX,y:signY});
 	}
-	else if(value<=360 && value > 270) {
+	//fourth zone
+	else if(value>=0 && value<=90) {
 		signX = 1;
 		signY = 1;
 		signXY.push({x:signX,y:signY});
@@ -54,6 +66,7 @@ function getSign(value) {
 }
 
 //convert input into logical value
+//NOT USED
 function checkDegreeSign(degree) {
 	if(degree >= 0) {
 		return degree;
@@ -63,30 +76,68 @@ function checkDegreeSign(degree) {
 	}
 }
 
-//NEED TO MEASURE SPEED INTO PIXEL PER SECOND 
-function onClick(e) {
-	a = e.pageX;
-    b = e.pageY;
+function createParticle(a0,b0,af,bf) {
+	// var x0 = e.pageX;
+ 	// var y0 = e.pageY;
 
-    //get size in radius
+ 	//Get origin coorinate x and y
+ 	var x0 = a0;
+ 	var y0 = b0;
+
+ 	//calculate difference from origin to destination
+ 	var da = (af - a0);			//x-axis
+ 	var db = (bf - b0);			//y-axis
+ 	//calculate degree and decide sign for direction
+ 	var degree = getDeg(Math.atan2(db,da));
+ 	var sign = getSign(degree);
+
+ 	var sFactor = Math.sqrt(Math.pow(w,2)+Math.pow(h,2));
+ 	//calculate velocity component x and y 
+ 	//note: the dragging length divided by the screen size decideds the speed
+ 	var va = ((Math.abs(af - a0)/sFactor)*VLC_SCALE) * sign.x;
+ 	var vb = ((Math.abs(bf - b0)/sFactor)*VLC_SCALE) * sign.y;
+
+    //get size in radius, SIZE DECIDES COLOR
     var size = Number(document.getElementById("radius").value);
 
-    //get ForceField radius
+    //get ForceField radius, default = 0, max = infinity
     var radius = Number(document.getElementById("maxRadius").value);
 
     //get speed and radian, find velocity in x and y
-    var speed = Number(document.getElementById("speed").value);
-    var degree = checkDegreeSign(Number((document.getElementById("angle").value)))%360;
-    var sign = getSign(degree);
+    // var speed = Number(document.getElementById("speed").value);
+    // var degree = checkDegreeSign(Number((document.getElementById("angle").value)))%360;
+    // var sign = getSign(degree);
     //Converting speed and degree into x and y components, also check p or n sign of direction
-    da = Math.sqrt((Math.pow(speed,2))/(1+Math.pow(Math.tan(getRad(degree)),2)));
-    db = Math.sqrt(Math.pow(speed,2)-Math.pow(da,2));
-    da = da * sign.x;	//change of x 	default = original speed and direction(no acceleration)
-    db = db * sign.y;	//change of y
+    // da = Math.sqrt((Math.pow(speed,2))/(1+Math.pow(Math.tan(getRad(degree)),2)));
+    // db = Math.sqrt(Math.pow(speed,2)-Math.pow(da,2));
+    // da = da * sign.x;	//change of x 	default = original speed and direction(no acceleration)
+    // db = db * sign.y;	//change of y
 
     //Save new particle data
     //Each particle has value of: x, y, dx, dy, size, forcefieldR, acceleration, gravity
-    particles.push({x:a, y:b, dx:da, dy:db, sz:size, r:radius});
+    particles.push({x:x0, y:y0, dx:va, dy:vb, sz:size, r:radius});
+}
+
+//Create particles color depends on size
+function colorParticle(sz) {
+	if(sz<1) {
+		return "#00ffff";
+	}
+	else if(sz<5) {
+		return "#fff";
+	}
+	else if(sz<10) {
+		return "#ffff00";
+	}
+	else if(sz<15) {
+		return "#ffb400";
+	}
+	else if(sz<20) {
+		return "#ffa000";
+	}
+	else { //sz>20
+		return "#ff6400";
+	}
 }
 
 //Draw on canvas
@@ -94,16 +145,16 @@ function drawParticles() {
 	for(var i = 0; i < particles.length; i++) {
 		//drawing the particles
 		ctx.beginPath();
-		ctx.arc(particles[i].x,particles[i].y,particles[i].sz,0,pi*2,false);
+		ctx.arc(particles[i].x,particles[i].y,particles[i].sz,0,PI*2,false);
 		ctx.closePath();
-		ctx.fillStyle="#fff";
+		ctx.fillStyle=colorParticle(particles[i].sz);
     	ctx.fill();
 
     	//drawing the force field of particle 
     	ctx.beginPath();
-    	ctx.arc(particles[i].x,particles[i].y,particles[i].r,0,pi*2,false);
+    	ctx.arc(particles[i].x,particles[i].y,particles[i].r,0,PI*2,false);
     	ctx.closePath();
-    	ctx.strokeStyle="rgba(255,255,255,0.5)";
+    	ctx.strokeStyle="rgba(0,255,255,0.5)";
     	ctx.stroke();
 	} 
 }
@@ -120,12 +171,16 @@ function update() {
 		particles[i].y+=particles[i].dy;
 	}
 	drawCross();
+	//draw line when holding
+	if(hold) {
+		drawVector();
+	}
 }
 
 //Event Listener coded on CSS to detect mouse moves
 function trackMouse(e) {
-	mouseX=e.pageX - canvas.offsetLeft;
-    mouseY=e.pageY - canvas.offsetTop;
+	mouseX = e.pageX - canvas.offsetLeft;
+    mouseY = e.pageY - canvas.offsetTop;
 }
 
 //Drawing X Y ruler on cursor
@@ -142,6 +197,36 @@ function drawCross() {
     ctx.strokeStyle="rgba(0,255,0,0.5)";
     ctx.stroke();
     ctx.closePath();
+}
+
+//variable to manipulate origin point to destination, and holding state of mouse
+var a0, b0, a, b, hold = false;
+
+//Upon holding down mouse click
+function dragging(e) {
+	a0 = e.pageX;
+	b0 = e.pageY;
+	hold = true;
+}
+
+//When release mouse click
+function dropping(e) {
+	a = e.pageX;
+	b = e.pageY;
+	hold = false;
+
+	//Create particle
+	createParticle(a0,b0,a,b);
+}
+
+function drawVector() {
+	//drawing line to where the cursor is from dragging source point
+	ctx.beginPath();
+	ctx.moveTo(a0,b0);
+	ctx.lineTo(mouseX,mouseY);
+	ctx.strokeStyle="rgba(0,255,0,1)";
+	ctx.stroke();
+	ctx.closePath();
 }
 
 function clearCanvas() {
